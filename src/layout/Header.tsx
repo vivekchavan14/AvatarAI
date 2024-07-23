1c26c67ccc98d019b0f32f21b32d4504db548df3
vivek <vivekchavan382@gmail.com>
2024-07-23 17:51:39 +0530

    done with home page

---
+++ src/layout/Header.tsx
@@ -0,0 +1,91 @@
+import {
+    Button,
+    Flex,
+    HStack,
+    Icon,
+    IconButton,
+    Text,
+    Tooltip,
+  } from "@chakra-ui/react";
+  import { signOut, useSession } from "next-auth/react";
+  import Link from "next/link";
+  import { HiLogout } from "react-icons/hi";
+  import { IoIosFlash } from "react-icons/io";
+  
+  const Header = () => {
+    const { data: session, status } = useSession();
+    return (
+      <Flex
+        width="100%"
+        flexDirection="column"
+        marginX="auto"
+        maxWidth="container.lg"
+        px="2"
+      >
+        <Flex justifyContent="space-between" py={4} as="footer">
+          <Flex
+            role="group"
+            as={Link}
+            href="/"
+            alignItems="center"
+            fontWeight="bold"
+            fontSize="2xl"
+          >
+            <Icon
+              transition="200ms all"
+              _groupHover={{ color: "brand.500" }}
+              as={IoIosFlash}
+            />
+            <Text display={{ base: "none", sm: "inherit" }}>AIAvatar.</Text>
+          </Flex>
+          <HStack spacing={1}>
+            <Button
+              as={Link}
+              href="/prompts"
+              colorScheme="beige"
+              variant="ghost"
+              size="sm"
+            >
+              Prompts
+            </Button>
+            {session ? (
+              <>
+                <Tooltip hasArrow label="Public Gallery">
+                  <Button
+                    href={`/gallery/${session.userId}`}
+                    as={Link}
+                    colorScheme="beige"
+                    variant="ghost"
+                    size="sm"
+                  >
+                    My Gallery
+                  </Button>
+                </Tooltip>
+                <Button href="/dashboard" as={Link} variant="brand" size="sm">
+                  Dashboard
+                </Button>
+                <Tooltip hasArrow label="Logout">
+                  <IconButton
+                    aria-label="logout"
+                    icon={<HiLogout />}
+                    size="sm"
+                    colorScheme="biege"
+                    variant="ghost"
+                    onClick={() => {
+                      signOut({ callbackUrl: "/" });
+                    }}
+                  />
+                </Tooltip>
+              </>
+            ) : (
+              <Button href="/login" as={Link} variant="brand" size="sm">
+                Login
+              </Button>
+            )}
+          </HStack>
+        </Flex>
+      </Flex>
+    );
+  };
+  
+  export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from "@/styles/theme";
+import Footer from "@/layout/Footer";
+import Header from "@/layout/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+  <SessionProvider>
   <ChakraProvider theme={theme}>
+    <Header />
     <Component {...pageProps} />
+    <Footer />
   </ChakraProvider>
+ </SessionProvider>  
   );
 }
